3b489a1a4bef65bad169e46ad873573165e0521a
damien <damienniyonshuti@gmail.com>
2022-01-13 13:24:13 -0600

    Ready for deployment to heroku.

---
+++ learning_logs/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from django.db import models
+from django.db.models import fields
+from django.forms import widgets
+from .models import Entry, Topic
+
+class TopicForm(forms.ModelForm):
+    class Meta:
+        model=Topic
+        fields=['text']
+        labels={'text': ''}
+
+class EntryForm(forms.ModelForm):
+    class Meta:
+        model=Entry
+        fields=["text"]
+        labels={'text': 'Entry:'}
+        widgets= {'text': forms.Textarea(attrs={'cols':80})}
+        
---
+++ learning_logs/urls.py
@@ -0,0 +1,31 @@
+"""Defines URL patterns for learning_logs."""
+
+from typing import NewType
+from django.conf.urls import url
+from django.urls import path
+from django.urls.resolvers import URLPattern
+
+from . import views
+
+app_name= "learning_logs"
+urlpatterns= [
+    #Home page
+    path('', views.index, name='index'),
+
+    #Page that shows all topics
+    path('topics/', views.topics, name='topics'),
+
+    #Detail page for a single topic.
+    path('topics/<int:topic_id>/', views.topic, name='topic'),
+
+    #Page for adding new topic
+    path('new_topic/', views.new_topic, name='new_topic'),
+
+    #Page for adding a new entry
+    path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'),   
+
+    #Page for editing an entry.
+    path ('edit_entry/<int:entry_id>/', views.edit_entry, name='edit_entry'), 
+]
+    
+
